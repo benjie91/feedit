fd9c62fa3cdfe52f4092d0d6a845bc783044e641
Benjie Cheong <benjie.cheong@gmail.com>
2019-09-27 00:55:21 +0800

    Setup base code for kotlin backend

---
+++ feedit-server/src/main/kotlin/io/feedit/server/controller/FeedbackController.kt
@@ -0,0 +1,25 @@
+package io.feedit.server.controller
+
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.*
+
+
+@RestController
+@RequestMapping("/api/feedback")
+class FeedbackController {
+
+  @PostMapping("/ingest")
+  fun ingestFeedback() {
+
+  }
+
+  @GetMapping("/retrieve/{systemName}")
+  fun retrieveSystemFeedback(@PathVariable systemName: String): String {
+    return ""
+  }
+
+  companion object {
+    @Suppress("JAVA_CLASS_ON_COMPANION")
+    private val logger = LoggerFactory.getLogger(javaClass.enclosingClass)
+  }
+}
