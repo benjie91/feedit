8c7e21a7e8463a2fcf9625795137c8e194bc579d
Benjie Cheong <benjie.cheong@gmail.com>
2019-10-13 16:54:02 +0800

    Add form to registration page

--- feedit-frontend/src/containers/RegistrationPage/index.js
+++ feedit-frontend/src/containers/RegistrationPage/index.js
@@ -1,12 +1,73 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 
+import { Card, Form, Button } from 'react-bootstrap';
 import PageHeader from '../../components/PageHeader';
 
 const RegistrationPage = () => {
+  const [registrationFormState, setRegistrationFormState] = useReducer(
+    (prevState, newState) => ({
+      ...prevState,
+      ...newState,
+    }),
+    { systemName: '', custodian: '' },
+  );
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = event => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+  };
+
   return (
-    <div>
+    <React.Fragment>
       <PageHeader header="System Registration" fontAwesomeIcon="coffee" />
-    </div>
+      <Card>
+        <Card.Body>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group controlId="registrationForm.SystemName">
+              <Form.Label>System Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter System Name"
+                value={registrationFormState.systemName}
+                onChange={event => {
+                  setRegistrationFormState({ systemName: event.target.value });
+                }}
+                required
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                Please enter a valid system name.
+              </Form.Control.Feedback>{' '}
+            </Form.Group>
+            <Form.Group controlId="registrationForm.custodian">
+              <Form.Label>Custodian</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Custodian Name"
+                value={registrationFormState.custodian}
+                onChange={event => {
+                  setRegistrationFormState({ custodian: event.target.value });
+                }}
+                required
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                Please enter a valid custodian name.
+              </Form.Control.Feedback>{' '}
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </React.Fragment>
   );
 };
 
