801dade61fc342570604d9d0ff12665cce1d1780
Benjie Cheong <benjie.cheong@gmail.com>
2019-10-13 16:53:41 +0800

    Edited Home Page

--- feedit-frontend/src/containers/HomePage/index.js
+++ feedit-frontend/src/containers/HomePage/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+
 import PageHeader from '../../components/PageHeader';
 
 const HomePage = () => {
   return (
     <div>
       <PageHeader header="Feedit System" fontAwesomeIcon="home" />
+      <p>
+        Feedit is a web application that helps you consolidate feedback from
+        your users. Just ask the right questions and hook up to feedit to see
+        these answered!
+      </p>
+      <p>
+        Before you can start analyzing on your user feedback, you need to
+        register your system with feedit.
+      </p>
     </div>
   );
 };
