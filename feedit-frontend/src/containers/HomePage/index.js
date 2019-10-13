373ea063942cb0b02e523d2d92bd612e4f78c811
Benjie Cheong <benjie.cheong@gmail.com>
2019-10-13 17:42:40 +0800

    Edited Home page to give more instruction

--- feedit-frontend/src/containers/HomePage/index.js
+++ feedit-frontend/src/containers/HomePage/index.js
@@ -1,20 +1,58 @@
 import React from 'react';
 
+import { Col, Row } from 'react-bootstrap';
+
 import PageHeader from '../../components/PageHeader';
+
+import feeditRegistrationURI from '../../assets/images/feedit-registration.png';
 
 const HomePage = () => {
   return (
     <div>
       <PageHeader header="Feedit System" fontAwesomeIcon="home" />
-      <p>
-        Feedit is a web application that helps you consolidate feedback from
-        your users. Just ask the right questions and hook up to feedit to see
-        these answered!
-      </p>
-      <p>
-        Before you can start analyzing on your user feedback, you need to
-        register your system with feedit.
-      </p>
+      <Row>
+        <Col xs={12}>
+          Feedit is a web application that helps you consolidate feedback from
+          your users. Just ask the right questions and hook up to feedit to see
+          these answered!
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        <Col xs={12} style={{ marginBottom: '20px' }}>
+          <h4>
+            Before you can start analyzing on your user feedback, you need to
+            register your system with feedit.
+          </h4>
+        </Col>
+        <Col xs={6} style={{ display: 'flex', justifyContent: 'center' }}>
+          <img
+            src={feeditRegistrationURI}
+            style={{
+              maxHeight: '300px',
+              border: '1px solid gray',
+            }}
+          />
+        </Col>
+        <Col xs={6}>
+          <div
+            style={{
+              height: '100%',
+              maxWidth: '400px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              textAlign: 'center',
+            }}
+          >
+            <span>
+              Click on the{' '}
+              <span style={{ color: 'red' }}>System Registration</span> button
+              on the side menu to register the system in feedit.
+            </span>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
