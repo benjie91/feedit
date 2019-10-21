c3ccac2756b03773366779c8820ec6611989507e
Brandon <brandon.twh@gmail.com>
2019-10-21 17:04:26 +0800

    Adding of axis labels

---
+++ feedit-frontend/src/components/BarChart/data.js
@@ -0,0 +1,10 @@
+export default [
+  { title: 'App01', value: 5 },
+  { title: 'App02', value: 3 },
+  { title: 'App03', value: 3 },
+  { title: 'App04', value: 3 },
+  { title: 'App05', value: 5 },
+  { title: 'App06', value: 3 },
+  { title: 'App07', value: 3 },
+  { title: 'App08', value: 3 },
+];
--- feedit-frontend/src/components/BarChart/index.js
+++ feedit-frontend/src/components/BarChart/index.js
@@ -1,47 +1,88 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import { scaleBand, scaleLinear } from 'd3-scale';
+import data from './data';
+import Axes from './Axes';
 
-class BarChart extends Component {
-  componentDidMount() {
-    const data = [2, 4, 2, 6, 8];
-    this.drawBarChart(data);
-  }
-
-  drawBarChart(data) {
-    const canvasHeight = 400;
-    const canvasWidth = 600;
-    const scale = 20;
-    const svgCanvas = d3
-      .select(this.refs.canvas)
-      .append('svg')
-      .attr('width', canvasWidth)
-      .attr('height', canvasHeight)
-      .style('border', '1px solid black');
-
-    svgCanvas
-      .selectAll('rect')
-      .data(data)
-      .enter()
-      .append('rect')
-      .attr('width', 40)
-      .attr('height', datapoint => datapoint * scale)
-      .attr('fill', 'orange')
-      .attr('x', (datapoint, iteration) => iteration * 45)
-      .attr('y', datapoint => canvasHeight - datapoint * scale);
-
-    // svgCanvas
-    //   .selectAll('text')
-    //   .data(data)
-    //   .enter()
-    //   .append('text')
-    //   .attr('x', (dataPoint, i) => i * 45 + 10)
-    //   .attr('y', (dataPoint, i) => canvasHeight - dataPoint * scale - 10)
-    //   .text(dataPoint => dataPoint);
+export default class BarChart extends Component {
+  constructor() {
+    super();
+    this.xScale = scaleBand();
+    this.yScale = scaleLinear();
   }
 
   render() {
-    return <div ref="canvas"></div>;
+    const margins = { top: 50, right: 20, bottom: 100, left: 60 };
+    const svgDimensions = { width: 800, height: 500 };
+
+    const maxValue = Math.max(...data.map(d => d.value));
+
+    // scaleBand type
+    const xScale = this.xScale
+      .padding(0.5)
+      // scaleBand domain should be an array of specific values
+      // in our case, we want to use movie titles
+      .domain(data.map(d => d.title))
+      .range([margins.left, svgDimensions.width - margins.right]);
+
+    // scaleLinear type
+    const yScale = this.yScale
+      // scaleLinear domain required at least two values, min and max
+      .domain([0, maxValue])
+      .range([svgDimensions.height - margins.bottom, margins.top]);
+
+    return (
+      <svg width={svgDimensions.width} height={svgDimensions.height}>
+        <Axes
+          scales={{ xScale, yScale }}
+          margins={margins}
+          svgDimensions={svgDimensions}
+        />
+      </svg>
+    );
   }
 }
 
-export default BarChart;
+//   componentDidMount() {
+//     const data = [2, 4, 2, 6, 8];
+//     this.drawBarChart(data);
+//   }
+//
+//   drawBarChart(data) {
+//     const canvasHeight = 400;
+//     const canvasWidth = 600;
+//     const scale = 20;
+//     const svgCanvas = d3
+//       .select(this.refs.canvas)
+//       .append('svg')
+//       .attr('width', canvasWidth)
+//       .attr('height', canvasHeight)
+//       .style('border', '1px solid black');
+//
+//     svgCanvas
+//       .selectAll('rect')
+//       .data(data)
+//       .enter()
+//       .append('rect')
+//       .attr('width', 40)
+//       .attr('height', datapoint => datapoint * scale)
+//       .attr('fill', 'orange')
+//       .attr('x', (datapoint, iteration) => iteration * 45)
+//       .attr('y', datapoint => canvasHeight - datapoint * scale);
+//
+//     // svgCanvas
+//     //   .selectAll('text')
+//     //   .data(data)
+//     //   .enter()
+//     //   .append('text')
+//     //   .attr('x', (dataPoint, i) => i * 45 + 10)
+//     //   .attr('y', (dataPoint, i) => canvasHeight - dataPoint * scale - 10)
+//     //   .text(dataPoint => dataPoint);
+//   }
+//
+//   render() {
+//     return <div ref="canvas"></div>;
+//   }
+// }
+//
+// export default BarChart;
