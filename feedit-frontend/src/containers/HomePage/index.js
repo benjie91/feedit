8768053af6513b89871893be4e1e87cc2c637737
Benjie Cheong <benjie.cheong@gmail.com>
2019-10-27 00:06:32 +0800

    Add additional info to home page

--- feedit-frontend/src/containers/HomePage/index.js
+++ feedit-frontend/src/containers/HomePage/index.js
@@ -5,6 +5,7 @@ import { Col, Row } from 'react-bootstrap';
 import PageHeader from '../../components/PageHeader';
 
 import feeditRegistrationURI from '../../assets/images/feedit-registration.png';
+import feedbitIngestRequest from '../../assets/images/feedit-ingest-request.png';
 
 const HomePage = () => {
   return (
@@ -19,12 +20,14 @@ const HomePage = () => {
       </Row>
       <br />
       <Row>
-        <Col xs={12} style={{ marginBottom: '20px' }}>
+        <Col xs={12} style={{ marginBottom: '60px' }}>
           <h4>
             Before you can start analyzing on your user feedback, you need to
             register your system with feedit.
           </h4>
         </Col>
+      </Row>
+      <Row style={{ marginBottom: '100px' }}>
         <Col xs={6} style={{ display: 'flex', justifyContent: 'center' }}>
           <img
             src={feeditRegistrationURI}
@@ -36,23 +39,52 @@ const HomePage = () => {
             alt="Feedit Registration Step"
           />
         </Col>
-        <Col xs={6}>
-          <div
-            style={{
-              height: '100%',
-              maxWidth: '400px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              textAlign: 'center',
-            }}
-          >
+        <Col
+          xs={6}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+          }}
+        >
+          <div style={{ maxWidth: '400px' }}>
             <span>
               Click on the{' '}
               <span style={{ color: 'red' }}>System Registration</span> button
               on the side menu to register the system in feedit.
             </span>
           </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          xs={6}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+          }}
+        >
+          <div style={{ maxWidth: '400px' }}>
+            <span>
+              Once registration is successful, you will receive an{' '}
+              <span style={{ color: 'red' }}>id</span>. Remember this id as this
+              will be used to transmit the feedback to feedit.
+            </span>
+          </div>
+        </Col>
+        <Col xs={6} style={{ display: 'flex', justifyContent: 'center' }}>
+          <img
+            src={feedbitIngestRequest}
+            style={{
+              maxHeight: '300px',
+              maxWidth: '-webkit-fill-available',
+              border: '1px solid gray',
+            }}
+            alt="Feedit Ingest Step"
+          />
         </Col>
       </Row>
     </div>
