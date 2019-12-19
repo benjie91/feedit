fe4f7205a54c8b917a63500871a3a305c5b6775f
Brandon <brandon.twh@gmail.com>
2019-12-19 15:53:40 +0800

    Implement the dropdown buttons

--- feedit-frontend/src/containers/DashboardPage/index.js
+++ feedit-frontend/src/containers/DashboardPage/index.js
@@ -7,11 +7,12 @@ import Bar from '../../components/BarChart';
 import Line from '../../components/LineChart';
 import Pie from '../../components/PieChart';
 import FeedbackDataGrid from '../../components/FeedbackDataGrid';
-
+import Dropdown from 'react-bootstrap/Dropdown';
 import { feedbackData } from '../../data/MockFeedbackData';
 import { feeditConfig } from '../../config/FeeditConfiguration';
 import { useJSONFetch } from '../../utils/ReactHooks';
 import LoadingPage from '../LoadingPage';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 // Todo: To remove all hardcoded information and refactor code
 const DashboardPage = () => {
@@ -33,38 +34,6 @@ const DashboardPage = () => {
   //
   // const systemName = systemData.map(data => data.systemName);
 
-  // let originalData = feedbackData;
-  // let lineData = feedbackData;
-  // let lineTitle = 'Past Month';
-  //
-  // const sysBtn = a => {
-  //   if (a === 'App01') {
-  //     feedbackData = feedbackData.filter(
-  //       feedbackData => feedbackData.sid === 'App01',
-  //     );
-  //     lineData = feedbackData.filter(
-  //       feedbackData => feedbackData.sid === 'App01',
-  //     );
-  //   } else if (a === 'App02') {
-  //     feedbackData = feedbackData.filter(
-  //       feedbackData => feedbackData.sid === 'App02',
-  //     );
-  //     lineData = feedbackData.filter(
-  //       feedbackData => feedbackData.sid === 'App02',
-  //     );
-  //   } else if (a === 'App03') {
-  //     feedbackData = feedbackData.filter(
-  //       feedbackData => feedbackData.sid === 'App03',
-  //     );
-  //     lineData = feedbackData.filter(
-  //       feedbackData => feedbackData.sid === 'App03',
-  //     );
-  //   } else {
-  //     feedbackData = originalData;
-  //     lineData = feedbackData;
-  //   }
-  // };
-
   const [type, setType] = useState('Week');
   const [sid, setSid] = useState('All');
 
@@ -85,19 +54,19 @@ const DashboardPage = () => {
         {/*    <Button onClick={() => {}}>All</Button>*/}
         {/*  </ButtonGroup>*/}
         {/*</Col>*/}
-        <Col md={4}>
-          <ButtonGroup aria-label="Basic example">
-            <Button onClick={() => setType('Week')}>Week</Button>
-            <Button onClick={() => setType('Year')}>Year</Button>
-          </ButtonGroup>
+        <Col md={2}>
+          <DropdownButton id="week/year" title="Week / Year">
+            <Dropdown.Item onClick={() => setType('Week')}>Week</Dropdown.Item>
+            <Dropdown.Item onClick={() => setType('Year')}>Year</Dropdown.Item>
+          </DropdownButton>
         </Col>
-        <Col md={4}>
-          <ButtonGroup aria-label="Basic example">
-            <Button onClick={() => setSid('All')}>All</Button>
-            <Button onClick={() => setSid('1')}>1</Button>
-            <Button onClick={() => setSid('2')}>2</Button>
-            <Button onClick={() => setSid('3')}>3</Button>
-          </ButtonGroup>
+        <Col md={2}>
+          <DropdownButton id="sid" title="System ID">
+            <Dropdown.Item onClick={() => setSid('All')}>All</Dropdown.Item>
+            <Dropdown.Item onClick={() => setSid('1')}>1</Dropdown.Item>
+            <Dropdown.Item onClick={() => setSid('2')}>2</Dropdown.Item>
+            <Dropdown.Item onClick={() => setSid('3')}>3</Dropdown.Item>
+          </DropdownButton>
         </Col>
       </Row>
       <Row style={{ marginBottom: '20px' }}>
