8842f7a91c1f99d45e3c63a01461d79f7f91b7f4
Brandon <brandon.twh@gmail.com>
2020-01-02 10:36:12 +0800

    Putting CSS files into feedit-theme and enabling resizable for charts

--- feedit-frontend/src/containers/DashboardPage/index.js
+++ feedit-frontend/src/containers/DashboardPage/index.js
@@ -3,9 +3,6 @@ import React, { useState } from 'react';
 import { Responsive as ResponsiveGridLayout } from 'react-grid-layout';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 
-import './react-grid-layout/css/styles.css';
-import './react-resizable/css/styles.css';
-
 import {
   Col,
   Row,
@@ -216,14 +213,10 @@ const DashboardPage = () => {
       <ResponsiveGridLayout
         className="layout"
         layouts={layouts}
-        // cols={12}
         breakpoints={{ lg: 1200, md: 996, sm: 768 }}
         cols={{ lg: 12, md: 10, sm: 6 }}
-        // style={{ marginBottom: '20px' }}
-        isResizable={false}
       >
         <div key="a">
-          {/*<Bar feedbackData={feedbackData} />*/}
           <Bar
             systemData={systemData}
             feedbackData={feedbackData}
@@ -241,10 +234,7 @@ const DashboardPage = () => {
         </div>
         <div key="c">
           <h3>Unread</h3>
-          {/*<h1 style="color:Tomato"> {feedbackData.length} </h1>*/}
-          {/*(feedbackData.length>1000)*/}
           <h1 style={{ color: unreadColour }}> {feedbackData.length} </h1>
-          {/*<h1 style={{ color: 'Tomato' }}> {feedbackData.length} </h1>*/}
         </div>
       </ResponsiveGridLayout>
 
